32f6377b05540d3faba6ad81b77fb2b7179f5020
Juan Salazar <juan.salazar@mundi.io>
2022-03-01 23:49:10 -0500

    add React Context

---
+++ src/App/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from 'react';
+import { TodoProvider } from '../TodoContext';
+import { AppUI } from './AppUI';
+
+function App() {
+  return (
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
+  );
+}
+
+export default App;
